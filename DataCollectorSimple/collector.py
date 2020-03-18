aa938c76e740d2d1fe956ad2d89ec79b07018f12
Tri0de <best108@uw.edu>
2020-03-18 15:18:55 -0700

    Finally, collecting data

--- DataCollectorSimple/collector.py
+++ DataCollectorSimple/collector.py
@@ -1,9 +1,9 @@
 import serial
-import numpy as np
+import time
+import json
 
 mag_port = 'COM11'
-
-mag = serial.Serial(mag_port, 115200)
+mag = serial.Serial(mag_port, 500000)
 
 
 def print_line(serial_port):
@@ -12,11 +12,18 @@ def print_line(serial_port):
     print(decoded_bytes)
 
 
-def read_mag_data(serial_port):
+def read_mag_data(serial_port, data_list):
     ser_bytes = serial_port.readline()
     decoded_str = ser_bytes[0:len(ser_bytes)-2].decode('utf-8')
     measurements = decoded_str.split(':')
-    print(measurements[0] + ':' + measurements[1] + ':' + measurements[2] + ':' + measurements[3])
+    
+    data_map = {}
+    for i in range(0, 4):
+        values = measurements[i].split(',')
+        data_map[i] = (float(values[1]), float(values[2]), float(values[3]))
+    
+    data_list.append(data_map)
+    # print(measurements[0] + ':' + measurements[1] + ':' + measurements[2] + ':' + measurements[3])
 
 
 print_line(mag)
@@ -25,10 +32,21 @@ mag.write('9'.encode('ascii'))
 
 mag.flushInput()
 
+
+data_samples = []
+data_json = {}
+data_json['samples'] = data_samples;
+data_json['start'] = time.time()
 while True:
     try:
-        read_mag_data(mag);
+        read_mag_data(mag, data_samples);
     except KeyboardInterrupt as e:
+        # Record the end time
+        data_json['end'] = time.time()
+        # Save the data        
+        with open('data_' + str(data_json['end']) + '.json', 'w') as outfile:
+            json.dump(data_json, outfile)
+        print('Data rate of ' + str(len(data_json['samples']) / (data_json['end'] - data_json['start'])))
         exit()
     except:
         pass
