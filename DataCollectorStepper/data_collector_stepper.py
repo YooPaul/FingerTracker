dc2b1f44a6e63bea7a2f95f17b074efccedc788a
Tri0de <best108@uw.edu>
2020-03-19 02:24:05 -0700

    Automatic data collection!

--- DataCollectorStepper/data_collector_stepper.py
+++ DataCollectorStepper/data_collector_stepper.py
@@ -14,7 +14,7 @@ def print_line(serial_port):
     print(decoded_bytes)
 
 
-def read_mag_data(serial_port, data_list):
+def read_mag_data(serial_port):
     ser_bytes = serial_port.readline()
     decoded_str = ser_bytes[0:len(ser_bytes)-2].decode('utf-8')
     measurements = decoded_str.split(':')
@@ -24,33 +24,59 @@ def read_mag_data(serial_port, data_list):
         values = measurements[i].split(',')
         data_map[i] = (float(values[1]), float(values[2]), float(values[3]))
     
-    data_list.append(data_map)
+    data_map['time'] = time.time()
+    return data_map
     # print(measurements[0] + ':' + measurements[1] + ':' + measurements[2] + ':' + measurements[3])
 
 
 print_line(mag)
-
 mag.write('9'.encode('ascii'))
-
 mag.flushInput()
 
-stepper.write('moveToCenter'.encode('ascii')))
+
+print_line(stepper)
+stepper.write('moveToCenter'.encode('ascii'))
+stepper.flushInput()
+print_line(stepper)
 
 
 data_samples = []
 data_json = {}
 data_json['samples'] = data_samples;
 data_json['start'] = time.time()
-while True:
-    try:
-        read_mag_data(mag, data_samples);
-    except KeyboardInterrupt as e:
-        # Record the end time
-        data_json['end'] = time.time()
-        # Save the data        
-        with open('data_' + str(data_json['end']) + '.json', 'w') as outfile:
-            json.dump(data_json, outfile)
-        print('Data rate of ' + str(len(data_json['samples']) / (data_json['end'] - data_json['start'])))
-        exit()
-    except:
-        pass
+
+
+step_size = 500
+
+data_json['stepsize'] = step_size
+
+current_x_pos = 0
+current_y_pos = 0
+for x in range(-2, 3):
+    for y in range(-2, 3):
+        step_dif_x = (x - current_x_pos) * step_size
+        step_dif_y = (y - current_y_pos) * step_size
+        current_x_pos = x
+        current_y_pos = y
+        
+        # First move the stepper motors
+        stepper.write(('moveTo ' + str(-step_dif_x) + ' ' + str(-step_dif_y)).encode('ascii'))
+        stepper.flushInput()
+        print_line(stepper)
+        
+        # Then make the measurements
+        print('Measuring position at ' + str(x) + ' ' + str(y))
+        samples_list = []
+        for i in range(0, 500):
+            data_map = read_mag_data(mag)
+            samples_list.append(data_map)
+        
+        data_json[str(-x * step_size) + ' ' + str(-y * step_size)] = samples_list
+        
+
+# Record the end time
+data_json['end'] = time.time()
+# Save the data        
+with open('data_' + str(data_json['end']) + '.json', 'w') as outfile:
+    json.dump(data_json, outfile)
+print('Data rate of ' + str(len(data_json['samples']) / (data_json['end'] - data_json['start'])))
